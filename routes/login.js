a73250772580e211aceaf8ab63e4a05ad5f86d1a
Harry McDonagh <harrymcdonagh82@gmail.com>
2024-06-08 20:26:03 +0100

    +Form Layouts & Login and Register pages

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
 const router = express.Router()
 
 router.get('/login', (req,res)=>{
-    res.render('login/login')
+    res.render('login/login',{
+        layout: 'layouts/form-layout',
+        formContent: fs.readFileSync(path.resolve(__dirname, '..', 'views', 'partials', 'login-form.ejs'))
+    })
 })
 
 module.exports = router
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const fs = require('fs');
+const path = require('path');
 const router = express.Router()
 
 router.get('/register', (req,res)=>{
-    res.render('register/register')
+    res.render('register/register',{
+        layout: 'layouts/form-layout',
+        formContent: fs.readFileSync(path.resolve(__dirname, '..', 'views', 'partials', 'register-form.ejs'))
+    })
 })
 
 module.exports = router
